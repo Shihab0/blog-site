011765a63682d312dfac8d5ff65325254922d49d
Shihab0 <ataharshihab1@gmail.com>
2023-10-01 21:35:36 +0600

    added fetured for desktop

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,8 +1,31 @@
 import React from "react";
 import styles from "./featured.module.css";
+import Image from "next/image";
 
 const Featured = () => {
-  return <div>featured</div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>
+        <b>Hey, Dorpon here!</b> Discover my stories and creative ideas.
+      </h1>
+      <div className={styles.post}>
+        <div className={styles.imageContainer}>
+          <Image src="/p1.jpeg" alt="" fill className={styles.image} />
+        </div>
+        <div className={styles.textContainer}>
+          <h1 className={styles.postTitle}>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo
+          </h1>
+          <p className={styles.postDesc}>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi
+            dignissimos quis reprehenderit facilis suscipit nesciunt eius eum
+            tenetur, dolorum fuga! Harum alias necessitatibus aperiam veritatis.
+          </p>
+          <button className={styles.button}>Read More</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Featured;
